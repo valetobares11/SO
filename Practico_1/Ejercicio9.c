7e7122181a93c8db3e267fc4c37af18b9d66e519
valetobares11 <valetobares11@gmail.com>
2022-03-22 14:05:30 -0300

    algunas modificaciones

--- Practico_1/Ejercicio9.c
+++ Practico_1/Ejercicio9.c
@@ -8,16 +8,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define AND "&&"
+#define OR "||"
+#define BACKGROUND_EXECUTION "&"
+#define SECUENTIAL_EXECUTION ";"
+#define PIPE "|"
+#define REDIRECT1 ">"
+#define REDIRECT2 "<"
+#define REDIRECT3 ">>"
+
+
 int and(char* cmd1, char* cmd2) // p && q
 {
+	char* cmd[2];
+	cmd[1]=0;
     int status=0;
     if (fork() == 0){
-		execl(cmd1, cmd1, (char*) NULL);
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
 	wait(&status);
 	if (status==0){
 		if (fork()==0){
-			execl(cmd2, cmd2, (char*) NULL);
+			cmd[0] = cmd2;
+			execv(cmd2, cmd);
 		}
 		wait(&status);
 	}
@@ -26,14 +40,18 @@ int and(char* cmd1, char* cmd2) // p && q
 
 int or(char* cmd1, char* cmd2) // p || q
 {
+	char* cmd[2];
+	cmd[1]=0;
     int status=0;
     if (fork() == 0){
-		execl(cmd1, cmd1, (char*) NULL);
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
 	wait(&status);
 	if (status!=0){
 		if (fork()==0){
-			execl(cmd2, cmd2, (char*) NULL);
+			cmd[0] = cmd2;
+			execv(cmd2, cmd);
 		}
 		wait(&status);
 	}
@@ -42,12 +60,16 @@ int or(char* cmd1, char* cmd2) // p || q
 
 int background_execution(char* cmd1, char* cmd2) // p & q
 {
+	char* cmd[2];
+	cmd[1]=0;
     int status=0;
     if (fork() == 0){
-		execl(cmd1, cmd1, (char*) NULL);
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
     if (fork()==0){
-			execl(cmd2, cmd2, (char*) NULL);
+			cmd[0] = cmd2;
+			execv(cmd2, cmd);
 		}
 	wait(&status);
     wait(&status);
@@ -56,19 +78,25 @@ int background_execution(char* cmd1, char* cmd2) // p & q
 
 int sequential_execution(char* cmd1, char* cmd2) // p ; q
 {
+	char* cmd[2];
+	cmd[1]=0;
     int status=0;
     if (fork() == 0){
-		execl(cmd1, cmd1, (char*) NULL);
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
 	wait(&status);
     if (fork() == 0){
-		execl(cmd2, cmd2, (char*) NULL);
+		cmd[0] = cmd2;
+		execv(cmd2, cmd);
 	}
 	wait(&status);
 }
 
-int op_pipe(char* cmd1, char* cmd2)// p | q	
+int pipe2(char* cmd1, char* cmd2)// p | q	
 {
+	char* cmd[2];
+	cmd[1]=0;
 	int  t[2];
 	int status=0;
     pipe(t);
@@ -76,13 +104,15 @@ int op_pipe(char* cmd1, char* cmd2)// p | q
 		close(1);
 		dup(t[1]);
 		close(t[0]);
-		execl(cmd1, cmd1, (char*) NULL);
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
 	if (fork()==0){
 		close(0);
 		dup(t[0]);
 		close(t[1]);
-		execl(cmd2, cmd2, (char*) NULL);
+		cmd[0] = cmd2;
+		execv(cmd2, cmd);
 	}
 	wait(&status);
 	wait(&status);
@@ -91,12 +121,15 @@ int op_pipe(char* cmd1, char* cmd2)// p | q
 
 int redirect1(char* cmd1, char* file_name)//p > file
 {
+	char* cmd[2];
+	cmd[1]=0;
 	int status;
 	if (fork() == 0){
 		int fd = open(file_name, O_WRONLY , O_CREAT);
 		close(1);
 		dup(fd);
-		execl(cmd1, cmd1, (char*) NULL);	
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
 	wait(&status);	
 	return status;
@@ -104,14 +137,16 @@ int redirect1(char* cmd1, char* file_name)//p > file
 
 int redirect2(char* cmd1, char* file_name) //p < file
 {
+	char* cmd[2];
+	cmd[1]=0;
 	int status;
 	if(fork() == 0) {
 		int fd =  open(file_name, O_RDONLY);
 		close(0);
 		dup(fd);
 		close(fd);
-		execl(cmd1, cmd1, (char*) NULL);
-		
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
 	wait(&status);
 	return status;
@@ -119,12 +154,15 @@ int redirect2(char* cmd1, char* file_name) //p < file
 
 int redirect3(char* cmd1, char* file_name) //p >> file
 {
+	char* cmd[2];
+	cmd[1]=0;
 	int status;
 	if(fork() == 0) {
 		int fd =  open(file_name, O_CREAT | O_APPEND | O_WRONLY);
 		close(1);
 		dup(fd);
-		execl(cmd1, cmd1, (char*) NULL);
+		cmd[0] = cmd1;
+		execv(cmd1,cmd);
 	}
 	wait(&status);
 	return 0;
@@ -136,23 +174,23 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "usage: ./myshell <cmd/file> <operator> <cmd/file>\n");
 		exit(-1);
 	} else {
-		if (strcmp(argv[2],"&&")==0)
+		if (strcmp(argv[2],AND)==0)
 			return and(argv[1],argv[3]);
-		if (strcmp(argv[2],"||")==0)
+		if (strcmp(argv[2],OR)==0)
 			return or(argv[1],argv[3]);
-		if (strcmp(argv[2],"&")==0)
+		if (strcmp(argv[2],BACKGROUND_EXECUTION)==0)
 			return background_execution(argv[1],argv[3]);
-		if (strcmp(argv[2],";")==0)
+		if (strcmp(argv[2],SECUENTIAL_EXECUTION)==0)
 			return sequential_execution(argv[1],argv[3]);
-		if (strcmp(argv[2],"|")==0)
-			return op_pipe(argv[1],argv[3]);
-		if (strcmp(argv[2],">")==0)
-			return redirect1(argv[1],argv[3]);
-		if (strcmp(argv[2],">>")==0)
-			return redirect3(argv[1],argv[3]);		
-		if (strcmp(argv[2],"<")==0)
-			return redirect2(argv[1],argv[3]);	 
+		if (strcmp(argv[2],PIPE)==0)
+			return pipe2(argv[1],argv[3]);
+		if (strcmp(argv[2],REDIRECT1)==0)
+			return redirect1(argv[1],argv[3]);	
+		if (strcmp(argv[2],REDIRECT2)==0)
+			return redirect2(argv[1],argv[3]);
+		if (strcmp(argv[2],REDIRECT3)==0)
+			return redirect3(argv[1],argv[3]);			 
 	}
- 
+	
 	return 0;
 }
