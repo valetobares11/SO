4499b8d22edb5f516618055618c7b9f2c84f88d3
valetobares11 <valetobares11@gmail.com>
2022-03-22 00:55:59 -0300

    algunas modificaciones

--- Practico_1/Ejercicio9.c
+++ Practico_1/Ejercicio9.c
@@ -1,7 +1,11 @@
-#include "stdio.h"
 #include <unistd.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int and(char* cmd1, char* cmd2)
@@ -85,8 +89,31 @@ int op_pipe(char* cmd1, char* cmd2)
 	return status;
 }
 
-
-
+int redirect1(char* cmd1, char* file_name)
+{
+	int status =0;
+	if (fork() == 0){
+		execl(cmd1, cmd1, (char*) NULL);
+	}
+	wait (&status);
+	int fd = open(file_name, O_WRONLY, O_CREAT);
+	close(1);
+	dup(fd);
+	execl(cmd1, cmd1, (char*) NULL);
+	return status;
+}
+int redirect2(char* cmd1, char* file_name)
+{
+	int status =0;
+	if (fork() == 0){
+		execl(cmd1, cmd1, (char*) NULL);
+	}
+	wait (&status);
+	int fd =  open(file_name, O_RDONLY);
+	close(0);
+	dup(fd);
+	return status;
+}
 int main(int argc, char* argv[])
 {
 	if (argc != 4) { 
@@ -101,7 +128,34 @@ int main(int argc, char* argv[])
 		return background_execution(argv[1],argv[3]);
     if (strcmp(argv[2],";")==0)
 		return sequential_execution(argv[1],argv[3]);
-      if (strcmp(argv[2],"|")==0)
-		return op_pipe(argv[1],argv[3]);         
+	if (strcmp(argv[2],"|")==0)
+		return op_pipe(argv[1],argv[3]);
+	if (strcmp(argv[2],">")==0)
+		return redirect1(argv[1],argv[3]);
+	if (strcmp(argv[2],"<")==0)
+		return redirect2(argv[1],argv[3]);	 
 	return 0;
 }
+
+/*p > file 
+1. fork()
+2. En el hijo
+	2.1 redirigir
+	2.2 exec
+3. En el padre
+	3.1 esperar por el hijo 
+	
+
+	int status =0;
+	if (fork() == 0){
+		exec("p");
+	}
+	wait (&status);
+	
+
+
+int fd = open(file, O_WRONLY, O_CREATE);
+close(1);
+dup(fd);
+exec("p");
+*/
